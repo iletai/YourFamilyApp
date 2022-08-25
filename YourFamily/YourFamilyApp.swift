0005db72b8c020eabf76ea27950a8f8fd40eed7d
Lê Quang Trọng Tài <iletai@hotmail.com>
2022-08-25 12:43:38 +0700

    + Initial project empty

---
+++ YourFamily/YourFamilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourFamilyApp.swift
+//  YourFamily
+//
+//  Created by Lê Quang Trọng Tài on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct YourFamilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
